5a6a8bed14eab044ad7edf0a1edb41e02ec0eaca
Minh Nguyen <pennalminh@gmail.com>
2026-01-14 01:52:03 +0700

    BE: allow all domain

--- BE/middlewares/allowCrossDomain.js
+++ BE/middlewares/allowCrossDomain.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 
 const allowCrossDomain = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.DOMAIN_FE);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
